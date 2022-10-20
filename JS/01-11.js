6cc8665c1917b4830967259fe999ad1fcfd24167
myplc <plctohmi@gmail.com>
2022-10-20 17:10:32 +0900

    오타수정

--- JS/01-11.js
+++ JS/01-11.js
@@ -1,4 +1,4 @@
-        let list = ";"
+        let list = "";
         list += "<table><tr><th>증감연산자</th><th>설명</th></tr>";
         list += "<tr><td>변수++</td><td>기존의 변수 값에 1을 더합니다(후위)</td>";
         list += "</tr><tr><td>++변수</td><td>기존의 변수 값에 1을 더합니다(전위)</td>";
