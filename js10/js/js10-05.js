dcb8f82594f8979f25dacc6ef6ce8536d2e33838
myplc <plctohmi@gmail.com>
2022-11-02 17:35:38 +0900

    삼항연산자 추가

---
+++ js10/js/js10-05.js
@@ -0,0 +1,30 @@
+let num=0
+        function updn(x){
+            console.log(x)
+            num=num+x
+            // if(x>0){x=x}else{x=-x}
+            x>0?x=x:x=-x
+            document.write(`<div>${num}</div><button onclick="updn(${x})">증가</button>            
+            <button onclick="updn(${-x})">감소</button>`)
+                    document.close()
+        }
+
+
+
+
+
+
+        
+    //     let num=0
+    //     function updn(x){
+    //         console.log(x)
+    //         num=num+x
+    //         if(x>0){   
+    //         document.write(`<div>${num}</div><button onclick="updn(${x})">증가</button>            
+    // <button onclick="updn(${-x})">감소</button>`)}
+    // else{
+    //     document.write(`<div>${num}</div><button onclick="updn(${-x})">증가</button>            
+    // <button onclick="updn(${x})">감소</button>`)
+    // }
+    //         document.close()
+    //     }     
